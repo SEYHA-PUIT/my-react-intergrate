f92b303264dc3429c94cd9c628b6102ee52aa7b0
SEYHA <rithseyhaonline@gmail.com>
2024-11-23 09:52:43 +0700

    Completely Intergrate HTML Template with React JS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Blog from "./Pages/Blog";
+import Cart from "./Pages/Cart";
+import Category from "./Pages/Category";
+import Checkout from "./Pages/Checkout";
+import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
+import SingleBlog from "./Pages/SingleBlog";
+import SingleProduct from "./Pages/SingleProduct";
+import Tracking from "./Pages/Tracking";
+import Elements from "./Pages/Elements";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/single-product" element={<SingleProduct />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/single-blog" element={<SingleBlog />} />
+          <Route path="/tracking" element={<Tracking />} />
+          <Route path="/elements" element={<Elements />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
